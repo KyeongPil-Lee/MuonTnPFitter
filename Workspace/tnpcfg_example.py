73681c85ca66f431378930db4aacf2807b678805
Kyoengpil Lee <kplee@cern.ch>
2018-09-11 19:19:19 +0200

    just change the order

--- Workspace/tnpcfg_example.py
+++ Workspace/tnpcfg_example.py
@@ -7,12 +7,12 @@ automator.configName = "TriggerEff.py"
 automator.inputTree = "/home/kplee/data1/TagProbe/101X/TnPTree_SingleMuon_Run2018Av123_Golden_315688_to_316271_DCSOnly_316272_to_316877.root"
 automator.isMC = False
 
-automator.doSkim = True
-automator.skimType = "101X_IsoMu24"
-
 automator.doRunSkim = True
 automator.firstRun = 316361
 automator.lastRun = 999999
+
+automator.doSkim = True
+automator.skimType = "101X_IsoMu24"
 
 automator.effList = [
 "IsoMu24_from_Tight2012_and_dBeta_015",
