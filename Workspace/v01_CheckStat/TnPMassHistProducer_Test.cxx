4ebf1c76b7e07b50407c309e3046c48bd22f004a
Kyoengpil Lee <kplee@cern.ch>
2018-10-30 19:13:19 +0100

    use type

--- Workspace/v01_CheckStat/TnPMassHistProducer_Test.cxx
+++ Workspace/v01_CheckStat/TnPMassHistProducer_Test.cxx
@@ -3,7 +3,7 @@
 void TnPMassHistProducer_Test()
 {
   Bool_t isMC = kFALSE;
-  DYTnP::HistProducer *producer = new DYTnP::HistProducer(isMC);
+  DYTnP::HistProducer *producer = new DYTnP::HistProducer("test", isMC);
   producer->AddNtuplePath("tnpZ_Data_1.root");
   // producer->SetVerboseLevel( kTRUE );
   producer->Produce();
