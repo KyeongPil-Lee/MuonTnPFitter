86deaf493f30e0dd15343319e4338aeecbfa5028
Kyoengpil Lee <kplee@cern.ch>
2018-11-12 17:38:17 +0100

    update job name

--- Workspace/tnpcfg_v04_NegBinCheck_NoNegBin.py
+++ Workspace/tnpcfg_v04_NegBinCheck_NoNegBin.py
@@ -1,7 +1,9 @@
 from Python.TnPAutomator import TnPAutomator
 
 automator = TnPAutomator()
-automator.jobName = "Validation_DataRunBtoF"
+
+# -- jobName: automatic guess from scriptname (tnpcfg_<jobName>.py)
+automator.jobName = sys.argv[0].split("tnpcfg_")[1].split(".py")[0]
 automator.configName = "TnPCfg_DY2016_Validation.py"
 
 automator.inputTree = "/scratch/kplee/TagProbe/TnPTree/2016/TnPTreeZ_LegacyRereco07Aug17_SingleMuon_Run2016BtoF_GoldenJSON.root"
