9417fbdb4145ed78baedc9d212b7af99a7b25349
Kyoengpil Lee <kplee@cern.ch>
2018-11-25 04:42:20 +0900

    merge single bin part

--- Template/ResultROOTFiles_v01/summary.sh
+++ Template/ResultROOTFiles_v01/summary.sh
@@ -1,11 +1,5 @@
 #!bin/bash
 
-cp ../Fitting_v01/*single.root ./SingleBin
-
-cd SingleBin
-python SaveCanvas_SingleBin.py
-
-cd ..
 source script_CollectROOTFiles.sh >&script_CollectROOTFiles.log
 source script_SummaryDir.sh
 
