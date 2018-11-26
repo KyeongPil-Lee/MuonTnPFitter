fb875d0903106dcfe7084a037351d5a79e5f171c
Kyoengpil Lee <kplee@cern.ch>
2018-11-26 15:34:43 +0100

    configs for cross-check with old rereco result

---
+++ Workspace/tnpcfg_v16_RunDepCheck_RunGH.py
@@ -0,0 +1,28 @@
+import sys
+from Python.TnPAutomator import TnPAutomator
+
+automator = TnPAutomator()
+
+# -- jobName: automatic guess from scriptname (tnpcfg_<jobName>.py)
+automator.jobName = sys.argv[0].split("tnpcfg_")[1].split(".py")[0]
+automator.configName = "TnPCfg_DY2016_RunDep.py"
+
+automator.inputTree = "/scratch/kplee/TagProbe/TnPTree/2016/TnPTreeZ_LegacyRereco07Aug17_SingleMuon_Run2016GtoH_GoldenJSON.root"
+automator.isMC = False
+
+automator.doRunSkim = False
+automator.firstRun = 0
+automator.lastRun = 99999
+
+automator.doSkim = True
+automator.skimType = "DY2016"
+
+automator.effList = [
+"HighPt_from_TM",
+"RelTrkIso_010_from_HighPt_and_TM",
+]
+
+automator.GenerateScript()
+
+
+
