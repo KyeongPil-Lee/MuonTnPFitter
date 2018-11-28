28505162c726ddf9b91e0b9e9e47800a9bcba89e
Kyoengpil Lee <kplee@cern.ch>
2018-11-28 15:33:46 +0100

    add MC results to calculate SF

---
+++ Workspace/tnpcfg_v25_IDOverTMTest_MC.py
@@ -0,0 +1,30 @@
+import sys
+from Python.TnPAutomator import TnPAutomator
+
+automator = TnPAutomator()
+
+# -- jobName: automatic guess from scriptname (tnpcfg_<jobName>.py)
+automator.jobName = sys.argv[0].split("tnpcfg_")[1].split(".py")[0]
+automator.configName = "TnPCfg_DY2016_IDOverTMTest.py"
+
+automator.inputTree = "/scratch/kplee/TagProbe/TnPTree/2016/TnPTreeZ_Summer16_DYLL_M50_aMCNLO_weightedToFULLData.root"
+automator.isMC = True
+
+automator.doRunSkim = False
+# automator.firstRun = 0
+# automator.lastRun = 999999
+
+automator.doSkim = True
+automator.skimType = "DY2016"
+
+automator.effList = [
+"HighPt_from_TM",
+"HighPt_from_TM,altSignalShape",
+"HighPt_from_TM,massRangeUp",
+"HighPt_from_TM,massRangeDown",
+]
+
+automator.GenerateScript()
+
+
+
