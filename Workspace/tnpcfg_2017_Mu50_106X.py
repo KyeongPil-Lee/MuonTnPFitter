ac25f696d63a27936944c2ba8b957d5459339874
kplee <kplee@cern.ch>
2019-05-15 21:19:14 +0900

    106X sample

---
+++ Workspace/tnpcfg_2017_Mu50_106X.py
@@ -0,0 +1,34 @@
+import sys
+from Python.TnPAutomator import TnPAutomator
+
+automator = TnPAutomator()
+automator.jobName = sys.argv[0].split("tnpcfg_")[1].split(".py")[0]
+automator.configName = "TriggerEff.py"
+
+automator.inputTree = "/scratch/kplee/TagProbe/TnPTree/2019/TnPTreeZ_1060pre2_RelValZMM.root"
+automator.isMC = True
+
+automator.doSkim = True
+automator.skimType = "2017_Mu50_NewHighPtID"
+
+automator.doRunSkim = False
+automator.firstRun = 0
+automator.lastRun = 999999
+
+automator.effList = [
+"Mu50_OR_OldMu100_OR_TkMu100_from_NewHighPtID_and_RelTrkIso_010",
+"Mu50_OR_OldMu100_OR_TkMu100_from_L1SingleMu22_and_NewHighPtID_and_RelTrkIso_010",
+]
+
+# automator.effList = [
+# "IsoMu24_from_Tight2012_and_dBeta_015",
+# "IsoMu24_from_L1SingleMu22_and_Tight2012_and_dBeta_015",
+# "L1SingleMu22_from_Tight2012_and_dBeta_015",
+# "L3_IsoMu24_from_L1SingleMu22_and_Tight2012_and_dBeta_015",
+# "IsoF_IsoMu24_from_L3_IsoMu24_and_L1SingleMu22_and_Tight2012_and_dBeta_015" ]
+
+# automator.GenerateScriptAndRun()
+automator.GenerateScript()
+
+
+
