5b3fac16e51a7461bf3f0b4a7b0ed529926b3dd2
kplee <kplee@cern.ch>
2019-06-14 16:53:27 +0900

    tnp configs

---
+++ Workspace/tnpcfg_106X_IsoMu24.py
@@ -0,0 +1,27 @@
+import sys
+from Python.TnPAutomator import TnPAutomator
+
+automator = TnPAutomator()
+automator.jobName = sys.argv[0].split("tnpcfg_")[1].split(".py")[0]
+automator.configName = "TriggerEff.py"
+
+automator.inputTree = "/scratch/kplee/TagProbe/TnPTree/2019/TnPTreeZ_1060For2018_RelValZMM_Reweighted.root"
+automator.isMC = True
+
+automator.doSkim = True
+automator.skimType = "101X_IsoMu24"
+
+automator.doRunSkim = False
+automator.firstRun = 0
+automator.lastRun = 999999
+
+automator.effList = [
+"IsoMu24_from_Tight2012_and_dBeta_015",
+"IsoMu24_from_L1SingleMu22_and_Tight2012_and_dBeta_015",
+"L1SingleMu22_from_Tight2012_and_dBeta_015",
+]
+
+automator.GenerateScript()
+
+
+
