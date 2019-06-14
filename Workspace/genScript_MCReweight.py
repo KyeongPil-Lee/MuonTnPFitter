2c6b5c5004e61aa12a3ef273c4f554202ca5fd6e
kplee <kplee@cern.ch>
2019-06-14 16:43:08 +0900

    script generator for weight

--- Workspace/genScript_MCReweight.py
+++ Workspace/genScript_MCReweight.py
@@ -5,17 +5,17 @@ generator = ScriptGenerator()
 generator.reweightCode = "$MUONTNP_PATH/Template/Input/addWeights.cxx"
 
 # -- path: must be absolute path
-generator.scriptName = "script_Reweight_94X.sh"
-generator.inputData = "/scratch/kplee/TagProbe/TnPTree/2017/TnPTreeZ_17Nov2017_SingleMuon_Run2017All_Incomplete_20190527.root"
-generator.inputMC   = "/scratch/kplee/TagProbe/TnPTree/2017/TnPTreeZ_94X_DYJetsToLL_M50_MadgraphMLM_Incomplete_20190527.root"
-generator.outputMC  = "/scratch/kplee/TagProbe/TnPTree/2017/TnPTreeZ_94X_DYJetsToLL_M50_MadgraphMLM_Incomplete_20190527_Reweighted.root"
+generator.scriptName = "script_Reweight_102X.sh"
+generator.inputData = "/scratch/kplee/TagProbe/TnPTree/2018/TnPTreeZ_17Sep2018_SingleMuon_Run2018All_Incomplete_20190527.root"
+generator.inputMC   = "/scratch/kplee/TagProbe/TnPTree/2018/TnPTreeZ_102XAutumn18_DYJetsToLL_M50_MadgraphMLM.root"
+generator.outputMC  = "/scratch/kplee/TagProbe/TnPTree/2018/TnPTreeZ_102XAutumn18_DYJetsToLL_M50_MadgraphMLM_Reweighted.root"
 generator.Register()
 generator.Generate()
 
 generator.scriptName = "script_Reweight_106X.sh"
-generator.inputData = "/scratch/kplee/TagProbe/TnPTree/2017/TnPTreeZ_17Nov2017_SingleMuon_Run2017All_Incomplete_20190527.root"
-generator.inputMC   = "/scratch/kplee/TagProbe/TnPTree/2019/TnPTreeZ_1060pre2_RelValZMM.root"
-generator.outputMC  = "/scratch/kplee/TagProbe/TnPTree/2019/TnPTreeZ_1060pre2_RelValZMM_Reweighted.root"
+generator.inputData = "/scratch/kplee/TagProbe/TnPTree/2018/TnPTreeZ_17Sep2018_SingleMuon_Run2018All_Incomplete_20190527.root"
+generator.inputMC   = "/scratch/kplee/TagProbe/TnPTree/2019/TnPTreeZ_1060For2018_RelValZMM.root"
+generator.outputMC  = "/scratch/kplee/TagProbe/TnPTree/2019/TnPTreeZ_1060For2018_RelValZMM_Reweighted.root"
 generator.Register()
 generator.Generate()
 
