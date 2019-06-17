0f8906bb8933d3427c765f7d5cb8cfbaf2c7f88c
kplee <kplee@cern.ch>
2019-06-17 19:59:26 +0900

    eff with nVtx > 25

---
+++ Workspace/tnpcfg_94X_IsoMu27_vtx25.py
@@ -0,0 +1,35 @@
+import sys
+from Python.TnPAutomator import TnPAutomator
+
+automator = TnPAutomator()
+automator.jobName = sys.argv[0].split("tnpcfg_")[1].split(".py")[0]
+automator.configName = "TriggerEff_vtx25.py"
+
+automator.inputTree = "/scratch/kplee/TagProbe/TnPTree/2017/TnPTreeZ_94X_DYJetsToLL_M50_MadgraphMLM_Incomplete_20190527_Reweighted.root"
+automator.isMC = True
+
+automator.doSkim = True
+automator.skimType = "2017_IsoMu27"
+
+automator.doRunSkim = False
+automator.firstRun = 0
+automator.lastRun = 999999
+
+automator.effList = [
+"IsoMu27_from_Tight2012_and_dBeta_015",
+"IsoMu27_from_L1SingleMu22_and_Tight2012_and_dBeta_015",
+"L1SingleMu22_from_Tight2012_and_dBeta_015",
+]
+
+# automator.effList = [
+# "IsoMu24_from_Tight2012_and_dBeta_015",
+# "IsoMu24_from_L1SingleMu22_and_Tight2012_and_dBeta_015",
+# "L1SingleMu22_from_Tight2012_and_dBeta_015",
+# "L3_IsoMu24_from_L1SingleMu22_and_Tight2012_and_dBeta_015",
+# "IsoF_IsoMu24_from_L3_IsoMu24_and_L1SingleMu22_and_Tight2012_and_dBeta_015" ]
+
+# automator.GenerateScriptAndRun()
+automator.GenerateScript()
+
+
+
---
+++ Workspace/tnpcfg_94X_Mu50_vtx25.py
@@ -0,0 +1,34 @@
+import sys
+from Python.TnPAutomator import TnPAutomator
+
+automator = TnPAutomator()
+automator.jobName = sys.argv[0].split("tnpcfg_")[1].split(".py")[0]
+automator.configName = "TriggerEff_vtx25.py"
+
+automator.inputTree = "/scratch/kplee/TagProbe/TnPTree/2017/TnPTreeZ_94X_DYJetsToLL_M50_MadgraphMLM_Incomplete_20190527_Reweighted.root"
+automator.isMC = True
+
+automator.doSkim = True
+automator.skimType = "2017_Mu50_NewHighPtID"
+
+automator.doRunSkim = False
+automator.firstRun = 0
+automator.lastRun = 999999
+
+automator.effList = [
+"Mu50_OR_OldMu100_OR_TkMu100_from_NewHighPtID_and_RelTrkIso_010",
+"Mu50_OR_OldMu100_OR_TkMu100_from_L1SingleMu22_and_NewHighPtID_and_RelTrkIso_010",
+]
+
+# automator.effList = [
+# "IsoMu24_from_Tight2012_and_dBeta_015",
+# "IsoMu24_from_L1SingleMu22_and_Tight2012_and_dBeta_015",
+# "L1SingleMu22_from_Tight2012_and_dBeta_015",
+# "L3_IsoMu24_from_L1SingleMu22_and_Tight2012_and_dBeta_015",
+# "IsoF_IsoMu24_from_L3_IsoMu24_and_L1SingleMu22_and_Tight2012_and_dBeta_015" ]
+
+# automator.GenerateScriptAndRun()
+automator.GenerateScript()
+
+
+
