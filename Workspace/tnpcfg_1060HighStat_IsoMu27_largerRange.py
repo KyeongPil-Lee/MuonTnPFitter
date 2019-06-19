bdd36ae2f6898abcd94c29370eec31cf20b0c0c2
kplee <kplee@cern.ch>
2019-06-19 16:45:10 +0900

    tnp run with larger range for the weight

---
+++ Workspace/tnpcfg_1060HighStat_IsoMu27_largerRange.py
@@ -0,0 +1,35 @@
+import sys
+from Python.TnPAutomator import TnPAutomator
+
+automator = TnPAutomator()
+automator.jobName = sys.argv[0].split("tnpcfg_")[1].split(".py")[0]
+automator.configName = "TriggerEff_largerRange.py"
+
+automator.inputTree = "/scratch/kplee/TagProbe/TnPTree/2019/TnPTreeZ_1060For2017_RelValZMM_HighStat_Reweighted.root"
+automator.isMC = True
+
+automator.doSkim = True
+automator.skimType = "2017_IsoMu27"
+
+automator.doRunSkim = False
+automator.firstRun = 0
+automator.lastRun = 999999
+
+automator.effList = [
+"IsoMu27_from_Tight2012_and_dBeta_015",
+"IsoMu27_from_L1SingleMu22_and_Tight2012_and_dBeta_015",
+"L1SingleMu22_from_Tight2012_and_dBeta_015",
+]
+
+# automator.effList = [
+# "IsoMu24_from_Tight2012_and_dBeta_015",
+# "IsoMu24_from_L1SingleMu22_and_Tight2012_and_dBeta_015",
+# "L1SingleMu22_from_Tight2012_and_dBeta_015",
+# "L3_IsoMu24_from_L1SingleMu22_and_Tight2012_and_dBeta_015",
+# "IsoF_IsoMu24_from_L3_IsoMu24_and_L1SingleMu22_and_Tight2012_and_dBeta_015" ]
+
+# automator.GenerateScriptAndRun()
+automator.GenerateScript()
+
+
+
