c356069d69760ff5b09ba8b623cef2c44cbfb4d4
kplee <kplee@cern.ch>
2019-06-26 21:31:58 +0900

    apply vtx 25 and eta restrictuion on tag

---
+++ Workspace/tnpcfg_94X_IsoMu27_vtx25_tagEndcap.py
@@ -0,0 +1,36 @@
+import sys
+from Python.TnPAutomator import TnPAutomator
+
+automator = TnPAutomator()
+automator.jobName = sys.argv[0].split("tnpcfg_")[1].split(".py")[0]
+automator.configName = "TriggerEff_vtx25_tagEndcap.py"
+
+automator.inputTree = "/scratch/kplee/TagProbe/MuonTnPFitter/v07_ULMCVal/Workspace/94X_IsoMu27_vtx25_v01/Input/Input_Final.root"
+automator.isMC = True
+automator.noMCWeight = True
+
+automator.doSkim = True
+automator.skimType = "2017_IsoMu27"
+
+automator.doRunSkim = False
+automator.firstRun = 0
+automator.lastRun = 999999
+
+automator.effList = [
+"IsoMu27_from_Tight2012_and_dBeta_015",
+"IsoMu27_from_L1SingleMu22_and_Tight2012_and_dBeta_015",
+"L1SingleMu22_from_Tight2012_and_dBeta_015",
+]
+
+# automator.effList = [
+# "IsoMu24_from_Tight2012_and_dBeta_015",
+# "IsoMu24_from_L1SingleMu22_and_Tight2012_and_dBeta_015",
+# "L1SingleMu22_from_Tight2012_and_dBeta_015",
+# "L3_IsoMu24_from_L1SingleMu22_and_Tight2012_and_dBeta_015",
+# "IsoF_IsoMu24_from_L3_IsoMu24_and_L1SingleMu22_and_Tight2012_and_dBeta_015" ]
+
+# automator.GenerateScriptAndRun()
+automator.GenerateScript()
+
+
+
