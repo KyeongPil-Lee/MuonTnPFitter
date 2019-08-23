5f91e4690f2bc6c8b16aab194c96f83c0ec65faf
kplee <kplee@cern.ch>
2019-08-23 22:22:49 +0900

    reweight to the last 5 /fb

--- Workspace/genScript_MCReweight.py
+++ Workspace/genScript_MCReweight.py
@@ -5,17 +5,17 @@ generator = ScriptGenerator()
 generator.reweightCode = "$MUONTNP_PATH/Template/Input/addWeights.cxx"
 
 # -- path: must be absolute path
-generator.scriptName = "script_Reweight_94X.sh"
-generator.inputData = "/scratch/kplee/TagProbe/TnPTree/2017/TnPTreeZ_17Nov2017_SingleMuon_Run2017All_Incomplete_20190527.root"
-generator.inputMC   = "/scratch/kplee/TagProbe/TnPTree/2017/TnPTreeZ_94X_DYJetsToLL_M50_MadgraphMLM_Incomplete_20190527.root"
-generator.outputMC  = "/scratch/kplee/TagProbe/TnPTree/2017/TnPTreeZ_94X_DYJetsToLL_M50_MadgraphMLM_Incomplete_20190527_Reweighted.root"
+generator.scriptName = "script_reweight_2016MC.sh"
+generator.inputData = "/scratch/kplee/TagProbe/TnPTree/2016/MuonTriggerPaper/TnPTreeZ_07Aug17_SingleMuon_Run2016All_last5Invfb.root"
+generator.inputMC   = "/scratch/kplee/TagProbe/TnPTree/2016/MuonTriggerPaper/TnPTreeZ_80X_DYJetsToLL_M50_MadgraphMLM.root"
+generator.outputMC  = "/scratch/kplee/TagProbe/TnPTree/2016/MuonTriggerPaper/TnPTreeZ_80X_DYJetsToLL_M50_MadgraphMLM_reweighted_last5Invfb.root"
 generator.Register()
 generator.Generate()
 
-generator.scriptName = "script_Reweight_106X.sh"
-generator.inputData = "/scratch/kplee/TagProbe/TnPTree/2017/TnPTreeZ_17Nov2017_SingleMuon_Run2017All_Incomplete_20190527.root"
-generator.inputMC   = "/scratch/kplee/TagProbe/TnPTree/2019/TnPTreeZ_1060pre2_RelValZMM.root"
-generator.outputMC  = "/scratch/kplee/TagProbe/TnPTree/2019/TnPTreeZ_1060pre2_RelValZMM_Reweighted.root"
+generator.scriptName = "script_Reweight_2018MC.sh"
+generator.inputData = "/scratch/kplee/TagProbe/TnPTree/2018/TnPTreeZ_17Sep2018_SingleMuon_Run2018All_Incomplete_20190702_last5Invfb.root"
+generator.inputMC   = "/scratch/kplee/TagProbe/TnPTree/2018/TnPTreeZ_102XAutumn18_DYJetsToLL_M50_MadgraphMLM.root"
+generator.outputMC  = "/scratch/kplee/TagProbe/TnPTree/2018/TnPTreeZ_102XAutumn18_DYJetsToLL_M50_MadgraphMLM_reweighted_last5Invfb.root"
 generator.Register()
 generator.Generate()
 
