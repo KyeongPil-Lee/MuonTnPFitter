d548b02ffc1e10ffad433cd782b7ecf35001f01c
kplee <kplee@cern.ch>
2020-02-19 16:53:54 +0900

    initial setup

--- Template/Input/GenScript_TnPTree.py
+++ Template/Input/GenScript_TnPTree.py
@@ -66,279 +66,31 @@ def Set_List_Cuts( Type, isMC, BranchList, AddList ):
 		AddList.append(["relTkIso < 0.10;relTkIso", "RelTrkIso_010", True])
 		AddList.append(["IsoMu24==1 or IsoTkMu24==1;IsoMu24;IsoTkMu24", "IsoMu24_OR_IsoTkMu24", True])
 
-	elif Type == "Zg_ForID":
-		CutDef_TagOfficial = "tag_IsoMu24==1 && abs(tag_eta) < 2.4 && tag_pt > 24.9"
-		CutDef_ID = "pt > 9.9 && pair_probeMultiplicity_Pt10_M60140 == 1 && mass > 76.9 && mass < 130.5"
-		CutDef = CutDef_TagOfficial + " && " + CutDef_ID
-
-	elif Type == "Zg_ForIso":
-		CutDef_TagOfficial = "tag_IsoMu24==1 && abs(tag_eta) < 2.4 && tag_pt > 24.9"
-		CutDef_ID = "pt > 9.9 && pair_probeMultiplicity_Pt10_M60140 == 1 && mass > 76.9 && mass < 130.5"
-		CutDef = CutDef_TagOfficial + " && " + CutDef_ID
-		
-		AddList.append(["relTkIso < 0.10;relTkIso", "RelTrkIso_010", True])
-
-	elif "Zg_ForTrig" in Type:
-		CutDef_Tag = "tag_IsoMu24==1 && tag_pt > 24.9 && mass > 69.5 && mass < 130.5" # -- tag condition -- #
-		CutDef_Probe = "relTkIso < 0.10 && pt > 9.9"
-		CutDef = CutDef_Tag + " && " + CutDef_Probe
-
-		AddList.append(["Mu50 == 1 or HLT_TkMu50 == 1;Mu50;HLT_TkMu50", "Mu50_OR_TkMu50", True])
-		AddList.append(["abs(tag_phi - phi) if abs(tag_phi - phi) < 3.1415926535 else 2*3.1415926535 - abs(tag_phi - phi);tag_eta;eta;phi;tag_phi", "pair_dPhi", False])
-		AddList.append(["pair_dPhi*(180/3.1415926535) if ((tag_eta > 0.9 and eta > 0.9) or (tag_eta < -0.9 and eta < -0.9)) else 999;pair_dPhi;tag_eta;eta;phi;tag_phi", "pair_dPhiPrimeDeg", False])
-
- 		if "Loose" in Type:
-			CutDef = CutDef + " && Loose"
-
-		if "HighPt" in Type:
-			CutDef = CutDef + " && HighPt"
-
-		# if "Period3" in Type:
-		# 	CutDef_AfterPhiFix = 'run > 278167 - 0.5'
-		# 	CutDef = CutDef + " && " + CutDef_AfterPhiFix
-
-		# elif "Period1" in Type:
-		# 	CutDef = CutDef + " && run < 274094 - 0.5"
-		# 	AddList.append(["abs(tag_phi - phi) if abs(tag_phi - phi) < 3.1415926535 else 2*3.1415926535 - abs(tag_phi - phi);tag_eta;eta;phi;tag_phi", "pair_dPhi", False])
-		# 	AddList.append(["pair_dPhi*(180/3.1415926535) if ((tag_eta > 0.9 and eta > 0.9) or (tag_eta < -0.9 and eta < -0.9)) else 999;pair_dPhi;tag_eta;eta;phi;tag_phi", "pair_dPhiPrimeDeg", False])
-
-		# elif "Period2" in Type:
-		# 	CutDef = CutDef + " && run > 274094 - 0.5 && run < 278167 - 0.5"
-		# 	AddList.append(["abs(tag_phi - phi) if abs(tag_phi - phi) < 3.1415926535 else 2*3.1415926535 - abs(tag_phi - phi);tag_eta;eta;phi;tag_phi", "pair_dPhi", False])
-		# 	AddList.append(["pair_dPhi*(180/3.1415926535) if ((tag_eta > 0.9 and eta > 0.9) or (tag_eta < -0.9 and eta < -0.9)) else 999;pair_dPhi;tag_eta;eta;phi;tag_phi", "pair_dPhiPrimeDeg", False])
-
-	elif "Zg_Test_NoUpperMW" in Type:
-		CutDef_Tag = "tag_IsoMu24==1 && tag_pt>25.9 && mass > 69.99" # -- tag condition -- #
-		CutDef_Probe = "relTkIso < 0.10 && pt > 9.9"
+	elif Type == "2016_IsoMu24":
+		CutDef_Tag = "tag_IsoMu24==1 && tag_pt > 25.9 && mass > 69.5 && mass < 130.5" # -- tag condition -- #
+		CutDef_Probe = "Tight2012 && combRelIsoPF04dBeta < 0.15" # -- offline selection -- #
 		CutDef = CutDef_Tag + " && " + CutDef_Probe
 
 		AddList.append(["IsoMu24 == 1 or IsoTkMu24 == 1;IsoMu24;IsoTkMu24", "IsoMu24_OR_IsoTkMu24", True])
-		AddList.append(["abs(tag_phi - phi) if abs(tag_phi - phi) < 3.1415926535 else 2*3.1415926535 - abs(tag_phi - phi);tag_eta;eta;phi;tag_phi", "pair_dPhi", False])
-		AddList.append(["pair_dPhi*(180/3.1415926535) if ((tag_eta > 0.9 and eta > 0.9) or (tag_eta < -0.9 and eta < -0.9)) else 999;pair_dPhi;tag_eta;eta;phi;tag_phi", "pair_dPhiPrimeDeg", False])
-
-		if "Loose" in Type:
-			CutDef = CutDef + " && Loose"
-
-		if "HighPt" in Type:
-			CutDef = CutDef + " && HighPt"
-
-	elif Type == "ForTrigFilters92X":
-		CutDef_Tag = "tag_IsoMu24==1 && tag_pt > 25.9 && mass > 69.5 && mass < 130.5" # -- tag condition -- #
-		CutDef_Probe = "Tight2012 && combRelIsoPF04dBeta < 0.15"
-		CutDef = CutDef_Tag + " && " + CutDef_Probe
-
-		BranchList.append( "hltL2fL1sSingleMu22L1f0L2Filtered10Q" )
-		BranchList.append( "hltL3fL1sSingleMu22L1f0L2f10QL3Filtered24Q" )
-		BranchList.append( "hltL3crIsoL1sSingleMu22L1f0L2f10QL3f24QL3trkIsoFiltered0p07" )
-		BranchList.append( "hltL3fL1sMu22f0TkFiltered24Q" )
-		BranchList.append( "hltL3fL1sMu22L1f0Tkf24QL3trkIsoFiltered0p07" )
-
-		AddList.append(["l1ptByQ >= 22 and l1qByQ == 12 and l1drByQ < 0.3;l1ptByQ;l1qByQ;l1drByQ", "L1_IsoMu24", True])
-		AddList.append(["hltL2fL1sSingleMu22L1f0L2Filtered10Q == 1;hltL2fL1sSingleMu22L1f0L2Filtered10Q", "L2_IsoMu24", True])
-		AddList.append(["hltL3fL1sSingleMu22L1f0L2f10QL3Filtered24Q == 1;hltL3fL1sSingleMu22L1f0L2f10QL3Filtered24Q", "L3_IsoMu24", True])
-		AddList.append(["hltL3crIsoL1sSingleMu22L1f0L2f10QL3f24QL3trkIsoFiltered0p07 == 1;hltL3crIsoL1sSingleMu22L1f0L2f10QL3f24QL3trkIsoFiltered0p07", "IsoF_IsoMu24", True])
-
-		AddList.append(["hltL3fL1sMu22f0TkFiltered24Q == 1;hltL3fL1sMu22f0TkFiltered24Q", "TkMuF_IsoTkMu24", True])
-		AddList.append(["hltL3fL1sMu22L1f0Tkf24QL3trkIsoFiltered0p07 == 1;hltL3fL1sMu22L1f0Tkf24QL3trkIsoFiltered0p07", "IsoF_IsoTkMu24", True])
-
-		AddList.append(["hltL3fL1sSingleMu22L1f0L2f10QL3Filtered24Q == 1 or hltL3fL1sMu22f0TkFiltered24Q == 1;hltL3fL1sSingleMu22L1f0L2f10QL3Filtered24Q;hltL3fL1sMu22f0TkFiltered24Q", "L3_IsoMu24_OR_TkMuF_IsoTkMu24", True])
-		AddList.append(["hltL3crIsoL1sSingleMu22L1f0L2f10QL3f24QL3trkIsoFiltered0p07 == 1 or hltL3fL1sMu22L1f0Tkf24QL3trkIsoFiltered0p07 == 1;hltL3crIsoL1sSingleMu22L1f0L2f10QL3f24QL3trkIsoFiltered0p07;hltL3fL1sMu22L1f0Tkf24QL3trkIsoFiltered0p07", "IsoF_IsoMu24_OR_IsoF_IsoTkMu24", True])
-
+		AddList.append(["l1ptByQ >= 22 and l1qByQ == 12 and l1drByQ < 0.3;l1ptByQ;l1qByQ;l1drByQ", "L1_SingleMu22", True])
 		AddList.append(["combRelIsoPF04dBeta < 0.15;combRelIsoPF04dBeta", "dBeta_015", True])
 
-		AddList.append(["IsoMu24==1 or IsoTkMu24==1;IsoMu24;IsoTkMu24", "IsoMu24_OR_IsoTkMu24", True])
-		AddList.append(["IsoMu27==1 or IsoTkMu27==1;IsoMu27;IsoTkMu27", "IsoMu27_OR_IsoTkMu27", True])
-
-
-	elif Type == "ForTrigFilters92XIterL3_IsoMu24":
-		CutDef_Tag = "tag_IsoMu24==1 && tag_pt > 25.9 && mass > 69.5 && mass < 130.5" # -- tag condition -- #
-		# CutDef_Tag = "tag_IsoMu27==1 && tag_pt > 28.9 && mass > 69.5 && mass < 130.5" # -- tag condition -- #
-		CutDef_Probe = "Tight2012 && combRelIsoPF04dBeta < 0.15"
-		CutDef = CutDef_Tag + " && " + CutDef_Probe
-
-		BranchList.append( "hltL2fL1sSingleMu22L1f0L2Filtered10Q" )
-		BranchList.append( "hltL3fL1sSingleMu22L1f0L2f10QL3Filtered24Q" )
-		BranchList.append( "hltL3crIsoL1sSingleMu22L1f0L2f10QL3f24QL3trkIsoFiltered0p07" )
-
-		AddList.append(["l1ptByQ >= 22 and l1qByQ == 12 and l1drByQ < 0.3;l1ptByQ;l1qByQ;l1drByQ", "L1_IsoMu24", True])
-		AddList.append(["hltL2fL1sSingleMu22L1f0L2Filtered10Q == 1;hltL2fL1sSingleMu22L1f0L2Filtered10Q", "L2_IsoMu24", True])
-		AddList.append(["hltL3fL1sSingleMu22L1f0L2f10QL3Filtered24Q == 1;hltL3fL1sSingleMu22L1f0L2f10QL3Filtered24Q", "L3_IsoMu24", True])
-		AddList.append(["hltL3crIsoL1sSingleMu22L1f0L2f10QL3f24QL3trkIsoFiltered0p07 == 1;hltL3crIsoL1sSingleMu22L1f0L2f10QL3f24QL3trkIsoFiltered0p07", "IsoF_IsoMu24", True])
-
+		# -- for further investigation on the isolation (e.g. Iso./L3)
+		# BranchList.append( "hltL3fL1sMu22L1f0L2f10QL3Filtered24Q" )
+		# BranchList.append( "hltL3fL1sMu22f0TkFiltered24Q" )
+		# AddList.append(["hltL3fL1sMu22L1f0L2f10QL3Filtered24Q == 1;hltL3fL1sMu22L1f0L2f10QL3Filtered24Q", "L3_IsoMu24", True])
 		# AddList.append(["hltL3fL1sMu22f0TkFiltered24Q == 1;hltL3fL1sMu22f0TkFiltered24Q", "TkMuF_IsoTkMu24", True])
-		# AddList.append(["hltL3fL1sMu22L1f0Tkf24QL3trkIsoFiltered0p07 == 1;hltL3fL1sMu22L1f0Tkf24QL3trkIsoFiltered0p07", "IsoF_IsoTkMu24", True])
+		# AddList.append(["L3_IsoMu24 == 1 or TkMuF_IsoTkMu24 == 1;L3_IsoMu24;TkMuF_IsoTkMu24", "L3_OR_TkMuF_IsoMu24", True])
 
-		# AddList.append(["hltL3fL1sSingleMu22L1f0L2f10QL3Filtered24Q == 1 or hltL3fL1sMu22f0TkFiltered24Q == 1;hltL3fL1sSingleMu22L1f0L2f10QL3Filtered24Q;hltL3fL1sMu22f0TkFiltered24Q", "L3_IsoMu24_OR_TkMuF_IsoTkMu24", True])
-		# AddList.append(["hltL3crIsoL1sSingleMu22L1f0L2f10QL3f24QL3trkIsoFiltered0p07 == 1 or hltL3fL1sMu22L1f0Tkf24QL3trkIsoFiltered0p07 == 1;hltL3crIsoL1sSingleMu22L1f0L2f10QL3f24QL3trkIsoFiltered0p07;hltL3fL1sMu22L1f0Tkf24QL3trkIsoFiltered0p07", "IsoF_IsoMu24_OR_IsoF_IsoTkMu24", True])
-
-		AddList.append(["combRelIsoPF04dBeta < 0.15;combRelIsoPF04dBeta", "dBeta_015", True])
-
-		# AddList.append(["IsoMu24==1 or IsoTkMu24==1;IsoMu24;IsoTkMu24", "IsoMu24_OR_IsoTkMu24", True])
-		# AddList.append(["IsoMu27==1 or IsoTkMu27==1;IsoMu27;IsoTkMu27", "IsoMu27_OR_IsoTkMu27", True])
-
-	elif Type == "ForTrigFilters92XIterL3_IsoMu27":
-		# CutDef_Tag = "tag_IsoMu24==1 && tag_pt > 25.9 && mass > 69.5 && mass < 130.5" # -- tag condition -- #
-		CutDef_Tag = "tag_IsoMu27==1 && tag_pt > 28.9 && mass > 69.5 && mass < 130.5" # -- tag condition -- #
-		CutDef_Probe = "Tight2012 && combRelIsoPF04dBeta < 0.15"
-		CutDef = CutDef_Tag + " && " + CutDef_Probe
-
-		BranchList.append( "hltL2fL1sMu22or25L1f0L2Filtered10Q" )
-		BranchList.append( "hltL3fL1sMu22Or25L1f0L2f10QL3Filtered27Q" )
-		BranchList.append( "hltL3crIsoL1sMu22Or25L1f0L2f10QL3f27QL3trkIsoFiltered0p07" )
-
-		AddList.append(["l1ptByQ >= 22 and l1qByQ == 12 and l1drByQ < 0.3;l1ptByQ;l1qByQ;l1drByQ", "L1_IsoMu27", True])
-		AddList.append(["hltL2fL1sMu22or25L1f0L2Filtered10Q == 1;hltL2fL1sMu22or25L1f0L2Filtered10Q", "L2_IsoMu27", True])
-		AddList.append(["hltL3fL1sMu22Or25L1f0L2f10QL3Filtered27Q == 1;hltL3fL1sMu22Or25L1f0L2f10QL3Filtered27Q", "L3_IsoMu27", True])
-		AddList.append(["hltL3crIsoL1sMu22Or25L1f0L2f10QL3f27QL3trkIsoFiltered0p07 == 1;hltL3crIsoL1sMu22Or25L1f0L2f10QL3f27QL3trkIsoFiltered0p07", "IsoF_IsoMu27", True])
-
-		AddList.append(["combRelIsoPF04dBeta < 0.15;combRelIsoPF04dBeta", "dBeta_015", True])
-
-	elif Type == "92X_Mu50":
-		CutDef_Tag = "tag_IsoMu27==1 && tag_pt > 28.9 && mass > 69.5 && mass < 130.5" # -- tag condition -- #
-		CutDef_Probe = "HighPt && relTkIso < 0.10" # -- offline selection -- #
-		CutDef = CutDef_Tag + " && " + CutDef_Probe
-
-		AddList.append(["l1ptByQ >= 22 and l1qByQ == 12 and l1drByQ < 0.3;l1ptByQ;l1qByQ;l1drByQ", "L1_IsoMu27", True])
-		AddList.append(["relTkIso < 0.10;relTkIso", "RelTrkIso_010", True])
-
-	# -- use same tag condition with 92X case - to compare with 2017 under exact same condition -- #
-	elif Type == "80X_IsoMu27":
-		CutDef_Tag = "tag_IsoMu27==1 && tag_pt > 28.9 && mass > 69.5 && mass < 130.5" # -- tag condition -- #
-		CutDef_Probe = "Tight2012 && combRelIsoPF04dBeta < 0.15" # -- offline selection -- #
-		CutDef = CutDef_Tag + " && " + CutDef_Probe
-
-		# BranchList.append( "hltL3crIsoL1sMu22Or25L1f0L2f10QL3f27QL3trkIsoFiltered0p09" )
-		# BranchList.append( "hltL3fL1sMu22Or25L1f0Tkf27QL3trkIsoFiltered0p09" )
-		# BranchList.append( "hltL3fL1sMu22Or25L1f0L2f10QL3Filtered27Q" )
-		# BranchList.append( "hltL3fL1sMu22Or25f0TkFiltered27Q" )
-
-		AddList.append(["IsoMu27 == 1 or IsoTkMu27 == 1;IsoMu27;IsoTkMu27", "IsoMu27_OR_IsoTkMu27", True])
-		AddList.append(["l1ptByQ >= 22 and l1qByQ == 12 and l1drByQ < 0.3;l1ptByQ;l1qByQ;l1drByQ", "L1_IsoMu27", True])
-
-		# -- TnP tree don't have filter information for IsoMu27 -- #
-		# # -- L3 -- #
-		# AddList.append(["hltL3fL1sMu22Or25L1f0L2f10QL3Filtered27Q == 1;hltL3fL1sMu22Or25L1f0L2f10QL3Filtered27Q", "L3_IsoMu27", True])
-		# AddList.append(["hltL3fL1sMu22Or25f0TkFiltered27Q == 1;hltL3fL1sMu22Or25f0TkFiltered27Q", "TkMuF_IsoTkMu27", True])
-		# AddList.append(["L3_IsoMu27 == 1 or TkMuF_IsoTkMu27 == 1;L3_IsoMu27;TkMuF_IsoTkMu27", "L3_OR_TkMuF_IsoMu27", True])
-
-		# -- IsoF -- #
-		# AddList.append(["hltL3crIsoL1sMu22Or25L1f0L2f10QL3f27QL3trkIsoFiltered0p09 == 1;hltL3crIsoL1sMu22Or25L1f0L2f10QL3f27QL3trkIsoFiltered0p09", "IsoF_IsoMu27", True])
-		# AddList.append(["hltL3fL1sMu22Or25L1f0Tkf27QL3trkIsoFiltered0p09 == 1;hltL3fL1sMu22Or25L1f0Tkf27QL3trkIsoFiltered0p09", "IsoF_IsoTkMu27", True])
-		# AddList.append(["IsoF_IsoMu27 == 1 or IsoF_IsoTkMu27 == 1;IsoF_IsoMu27;IsoF_IsoTkMu27", "IsoF_IsoMu27_OR_IsoF_IsoTkMu27", True])
-
-		AddList.append(["combRelIsoPF04dBeta < 0.15;combRelIsoPF04dBeta", "dBeta_015", True])
-
-	# -- use same tag condition with 92X case - to compare with 2017 under exact same condition -- #
-	elif Type == "80X_Mu50":
-		CutDef_Tag = "tag_IsoMu27==1 && tag_pt > 28.9 && mass > 69.5 && mass < 130.5" # -- tag condition -- #
-		CutDef_Probe = "Tight2012 && combRelIsoPF04dBeta < 0.15" # -- offline selection -- #
+	elif Type == "2016_Mu50":
+		CutDef_Tag = "tag_IsoMu24==1 && tag_pt > 25.9 && mass > 69.5 && mass < 130.5" # -- tag condition -- #
+		CutDef_Probe = "CutBasedIdGlobalHighPt_new > 0.5 && relTkIso < 0.10" # -- offline selection -- #
 		CutDef = CutDef_Tag + " && " + CutDef_Probe
 
 		AddList.append(["Mu50 == 1 or HLT_TkMu50 == 1;Mu50;HLT_TkMu50", "Mu50_OR_TkMu50", True])
-		AddList.append(["l1ptByQ >= 22 and l1qByQ == 12 and l1drByQ < 0.3;l1ptByQ;l1qByQ;l1drByQ", "L1_IsoMu27", True])
-		AddList.append(["relTkIso < 0.10;relTkIso", "RelTrkIso_010", True])
-
-	# -- use same tag condition with 92X case - to compare with 2017 under exact same condition -- #
-	elif Type == "80X_IsoMu24":
-		CutDef_Tag = "tag_IsoMu27==1 && tag_pt > 28.9 && mass > 69.5 && mass < 130.5" # -- tag condition -- #
-		CutDef_Probe = "HighPt && relTkIso < 0.10" # -- offline selection -- #
-		CutDef = CutDef_Tag + " && " + CutDef_Probe
-
-		BranchList.append( "hltL3fL1sMu22L1f0L2f10QL3Filtered24Q" )
-		BranchList.append( "hltL3fL1sMu22f0TkFiltered24Q" )
-
-		AddList.append(["IsoMu24 == 1 or IsoTkMu24 == 1;IsoMu24;IsoTkMu24", "IsoMu24_OR_IsoTkMu24", True])
-		AddList.append(["l1ptByQ >= 22 and l1qByQ == 12 and l1drByQ < 0.3;l1ptByQ;l1qByQ;l1drByQ", "L1_IsoMu27", True])
-		AddList.append(["hltL3fL1sMu22L1f0L2f10QL3Filtered24Q == 1;hltL3fL1sMu22L1f0L2f10QL3Filtered24Q", "L3_IsoMu24", True])
-		AddList.append(["hltL3fL1sMu22f0TkFiltered24Q == 1;hltL3fL1sMu22f0TkFiltered24Q", "TkMuF_IsoTkMu24", True])
-		AddList.append(["L3_IsoMu24 == 1 or TkMuF_IsoTkMu24 == 1;L3_IsoMu24;TkMuF_IsoTkMu24", "L3_OR_TkMuF_IsoMu24", True])
-		AddList.append(["combRelIsoPF04dBeta < 0.15;combRelIsoPF04dBeta", "dBeta_015", True])
-
-	elif Type == "92X_OldMu100":
-		CutDef_Tag = "tag_IsoMu27==1 && tag_pt > 28.9 && mass > 69.5 && mass < 130.5" # -- tag condition -- #
-		CutDef_Probe = "HighPt && relTkIso < 0.10" # -- offline selection -- #
-		CutDef = CutDef_Tag + " && " + CutDef_Probe
-
-		AddList.append(["l1ptByQ >= 22 and l1qByQ == 12 and l1drByQ < 0.3;l1ptByQ;l1qByQ;l1drByQ", "L1_IsoMu27", True])
-		AddList.append(["OldMu100 == 1 or TkMu100 == 1;OldMu100;TkMu100", "OldMu100_OR_TkMu100", True])
-		AddList.append(["Mu50 == 1 or OldMu100 == 1;Mu50;OldMu100", "Mu50_OR_OldMu100", True])
-		AddList.append(["Mu50 == 1 or TkMu100 == 1;Mu50;TkMu100", "Mu50_OR_TkMu100", True])
-
-		AddList.append(["relTkIso < 0.10;relTkIso", "RelTrkIso_010", True])
-
-	elif Type == "94X_IsoMu27":
-		CutDef_Tag = "tag_IsoMu27==1 && tag_pt > 28.9 && mass > 69.5 && mass < 130.5" # -- tag condition -- #
-		CutDef_Probe = "Tight2012 && combRelIsoPF04dBeta < 0.15"
-		CutDef = CutDef_Tag + " && " + CutDef_Probe
-
-		AddList.append(["combRelIsoPF04dBeta < 0.15;combRelIsoPF04dBeta", "dBeta_015", True])
-		AddList.append(["l1ptByQ >= 22 and l1qByQ == 12 and l1drByQ < 0.3;l1ptByQ;l1qByQ;l1drByQ", "L1_IsoMu27", True])
-
-	elif Type == "94X_Mu50":
-		CutDef_Tag = "tag_IsoMu27==1 && tag_pt > 28.9 && mass > 69.5 && mass < 130.5" # -- tag condition -- #
-		CutDef_Probe = "HighPt && relTkIso < 0.10"
-		CutDef = CutDef_Tag + " && " + CutDef_Probe
-
-		AddList.append(["relTkIso < 0.10;relTkIso", "RelTrkIso_010", True])
-		AddList.append(["l1ptByQ >= 22 and l1qByQ == 12 and l1drByQ < 0.3;l1ptByQ;l1qByQ;l1drByQ", "L1_IsoMu27", True])
-
-	elif Type == "94X_IsoMu27Filters":
-		CutDef_Tag = "tag_IsoMu27==1 && tag_pt > 28.9 && mass > 69.5 && mass < 130.5" # -- tag condition -- #
-		CutDef_Probe = "Tight2012 && combRelIsoPF04dBeta < 0.15"
-		CutDef = CutDef_Tag + " && " + CutDef_Probe
-
-		BranchList.append( "hltL3fL1sMu22Or25L1f0L2f10QL3Filtered27Q" )
-		BranchList.append( "hltL3crIsoL1sMu22Or25L1f0L2f10QL3f27QL3trkIsoFiltered0p07" )
-
-		AddList.append(["combRelIsoPF04dBeta < 0.15;combRelIsoPF04dBeta", "dBeta_015", True])
-		AddList.append(["l1ptByQ >= 22 and l1qByQ == 12 and l1drByQ < 0.3;l1ptByQ;l1qByQ;l1drByQ", "L1_IsoMu27", True])
-		AddList.append(["hltL3fL1sMu22Or25L1f0L2f10QL3Filtered27Q == 1;hltL3fL1sMu22Or25L1f0L2f10QL3Filtered27Q", "L3_IsoMu27", True])
-		AddList.append(["hltL3crIsoL1sMu22Or25L1f0L2f10QL3f27QL3trkIsoFiltered0p07 == 1;hltL3crIsoL1sMu22Or25L1f0L2f10QL3f27QL3trkIsoFiltered0p07", "IsoF_IsoMu27", True])
-
-	elif Type == "101X_IsoMu24Filters":
-		CutDef_Tag = "tag_IsoMu24==1 && tag_pt > 25.9 && mass > 69.5 && mass < 130.5" # -- tag condition -- #
-		CutDef_Probe = "Tight2012 && combRelIsoPF04dBeta < 0.15"
-		CutDef = CutDef_Tag + " && " + CutDef_Probe
-
-		BranchList.append( "hltL2fL1sSingleMu22L1f0L2Filtered10Q" )
-		BranchList.append( "hltL3fL1sSingleMu22L1f0L2f10QL3Filtered24Q" )
-		BranchList.append( "hltL3crIsoL1sSingleMu22L1f0L2f10QL3f24QL3trkIsoFiltered0p07" )
-
-		AddList.append(["combRelIsoPF04dBeta < 0.15;combRelIsoPF04dBeta", "dBeta_015", True])
-		AddList.append(["l1ptByQ >= 22 and l1qByQ == 12 and l1drByQ < 0.3;l1ptByQ;l1qByQ;l1drByQ", "L1_IsoMu24", True])
-
-		AddList.append(["hltL2fL1sSingleMu22L1f0L2Filtered10Q == 1;hltL2fL1sSingleMu22L1f0L2Filtered10Q", "L2_IsoMu24", True])
-		AddList.append(["hltL3fL1sSingleMu22L1f0L2f10QL3Filtered24Q == 1;hltL3fL1sSingleMu22L1f0L2f10QL3Filtered24Q", "L3_IsoMu24", True])
-		AddList.append(["hltL3crIsoL1sSingleMu22L1f0L2f10QL3f24QL3trkIsoFiltered0p07 == 1;hltL3crIsoL1sSingleMu22L1f0L2f10QL3f24QL3trkIsoFiltered0p07", "IsoF_IsoMu24", True])
-		
-
-	elif Type == "101X_Mu50":
-		CutDef_Tag = "tag_IsoMu24==1 && tag_pt > 25.9 && mass > 69.5 && mass < 130.5" # -- tag condition -- #
-		CutDef_Probe = "HighPt && relTkIso < 0.10"
-		CutDef = CutDef_Tag + " && " + CutDef_Probe
-
-		AddList.append(["relTkIso < 0.10;relTkIso", "RelTrkIso_010", True])
-		AddList.append(["l1ptByQ >= 22 and l1qByQ == 12 and l1drByQ < 0.3;l1ptByQ;l1qByQ;l1drByQ", "L1SingleMu22", True])
-
-
-	elif Type == "101X_Mu50_NewHighPtID":
-		CutDef_Tag = "tag_IsoMu24==1 && tag_pt > 25.9 && mass > 69.5 && mass < 130.5" # -- tag condition -- #
-		CutDef_Probe = "CutBasedIdGlobalHighPt_new > 0.5 && relTkIso < 0.10"
-		CutDef = CutDef_Tag + " && " + CutDef_Probe
-
-		AddList.append(["relTkIso < 0.10;relTkIso", "RelTrkIso_010", True])
-		AddList.append(["l1ptByQ >= 22 and l1qByQ == 12 and l1drByQ < 0.3;l1ptByQ;l1qByQ;l1drByQ", "L1SingleMu22", True])
+		AddList.append(["l1ptByQ >= 22 and l1qByQ == 12 and l1drByQ < 0.3;l1ptByQ;l1qByQ;l1drByQ", "L1_SingleMu22", True])
 		AddList.append(["CutBasedIdGlobalHighPt_new > 0.5;CutBasedIdGlobalHighPt_new", "NewHighPtID", True])
-
-
-	elif Type == "101X_IsoMu24":
-		CutDef_Tag = "tag_IsoMu24==1 && tag_pt > 25.9 && mass > 69.5 && mass < 130.5" # -- tag condition -- #
-		CutDef_Probe = "Tight2012 && combRelIsoPF04dBeta < 0.15"
-		CutDef = CutDef_Tag + " && " + CutDef_Probe
-
-		AddList.append(["combRelIsoPF04dBeta < 0.15;combRelIsoPF04dBeta", "dBeta_015", True])
-		AddList.append(["l1ptByQ >= 22 and l1qByQ == 12 and l1drByQ < 0.3;l1ptByQ;l1qByQ;l1drByQ", "L1SingleMu22", True])
-
+		AddList.append(["relTkIso < 0.10;relTkIso", "RelTrkIso_010", True])
 
 	return CutDef
 
--- Workspace/genScript_MCReweight.py
+++ Workspace/genScript_MCReweight.py
@@ -5,17 +5,17 @@ generator = ScriptGenerator()
 generator.reweightCode = "$MUONTNP_PATH/Template/Input/addWeights.cxx"
 
 # -- path: must be absolute path
-generator.scriptName = "script_Reweight_94X.sh"
-generator.inputData = "/scratch/kplee/TagProbe/TnPTree/2017/TnPTreeZ_17Nov2017_SingleMuon_Run2017All_Incomplete_20190527.root"
-generator.inputMC   = "/scratch/kplee/TagProbe/TnPTree/2017/TnPTreeZ_94X_DYJetsToLL_M50_MadgraphMLM_Incomplete_20190527.root"
-generator.outputMC  = "/scratch/kplee/TagProbe/TnPTree/2017/TnPTreeZ_94X_DYJetsToLL_M50_MadgraphMLM_Incomplete_20190527_Reweighted.root"
+generator.scriptName = "script_Reweight_106X.sh"
+generator.inputData = "/scratch/kplee/TagProbe/TnPTree/2016/MuonTriggerPaper/TnPTreeZ_07Aug17_SingleMuon_Run2016All_afterL1Fix.root"
+generator.inputMC   = "/scratch/kplee/TagProbe/TnPTree/2020/TnPTreeZ_RelValZMM_13UP16_UL16_HighStat.root"
+generator.outputMC  = "/scratch/kplee/TagProbe/TnPTree/2020/TnPTreeZ_RelValZMM_13UP16_UL16_HighStat_reweighted_afterL1Fix.root"
 generator.Register()
 generator.Generate()
 
-generator.scriptName = "script_Reweight_106X.sh"
-generator.inputData = "/scratch/kplee/TagProbe/TnPTree/2017/TnPTreeZ_17Nov2017_SingleMuon_Run2017All_Incomplete_20190527.root"
-generator.inputMC   = "/scratch/kplee/TagProbe/TnPTree/2019/TnPTreeZ_1060pre2_RelValZMM.root"
-generator.outputMC  = "/scratch/kplee/TagProbe/TnPTree/2019/TnPTreeZ_1060pre2_RelValZMM_Reweighted.root"
+generator.scriptName = "script_Reweight_106X_WrongL1.sh"
+generator.inputData = "/scratch/kplee/TagProbe/TnPTree/2016/MuonTriggerPaper/TnPTreeZ_07Aug17_SingleMuon_Run2016All_afterL1Fix.root"
+generator.inputMC   = "/scratch/kplee/TagProbe/TnPTree/2020/TnPTreeZ_RelValZMM_13UP16_UL16_HighStat_WrongL1.root"
+generator.outputMC  = "/scratch/kplee/TagProbe/TnPTree/2020/TnPTreeZ_RelValZMM_13UP16_UL16_HighStat_WrongL1_reweighted_afterL1Fix.root"
 generator.Register()
 generator.Generate()
 
