49fbd048925ef27c1f3a0b51452e657fd8ba9945
kplee <kplee@cern.ch>
2020-02-19 18:51:58 +0900

    tnp config for 106X

---
+++ Workspace/tnpcfg_IsoMu24_106X_WrongL1.py
@@ -0,0 +1,34 @@
+import sys
+from Python.TnPAutomator import TnPAutomator
+
+automator = TnPAutomator()
+automator.jobName = sys.argv[0].split("tnpcfg_")[1].split(".py")[0]
+automator.configName = "TriggerEff.py"
+
+automator.inputTree = "/scratch/kplee/TagProbe/TnPTree/2020/TnPTreeZ_RelValZMM_13UP16_UL16_HighStat_WrongL1_reweighted_afterL1Fix.root"
+automator.isMC = True
+
+automator.doSkim = True
+automator.skimType = "2016_IsoMu24"
+
+automator.doRunSkim = False
+automator.firstRun = 0
+automator.lastRun = 999999
+
+automator.effList = [
+"IsoMu24_OR_IsoTkMu24_from_Tight2012_and_dBeta_015",
+"L1SingleMu22_from_Tight2012_and_dBeta_015",
+"IsoMu24_OR_IsoTkMu24_from_L1SingleMu22_and_Tight2012_and_dBeta_015"]
+
+# automator.effList = [
+# "IsoMu24_from_Tight2012_and_dBeta_015",
+# "IsoMu24_from_L1SingleMu22_and_Tight2012_and_dBeta_015",
+# "L1SingleMu22_from_Tight2012_and_dBeta_015",
+# "L3_IsoMu24_from_L1SingleMu22_and_Tight2012_and_dBeta_015",
+# "IsoF_IsoMu24_from_L3_IsoMu24_and_L1SingleMu22_and_Tight2012_and_dBeta_015" ]
+
+# automator.GenerateScriptAndRun()
+automator.GenerateScript()
+
+
+
