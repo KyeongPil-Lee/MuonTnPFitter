af836ace064b39c0ebc6bf7a072de7a684f6cfcc
kplee <kplee@cern.ch>
2020-04-03 14:40:29 +0900

    gen weight codes

--- Workspace/genScript_MCReweight.py
+++ Workspace/genScript_MCReweight.py
@@ -5,24 +5,38 @@ generator = ScriptGenerator()
 generator.reweightCode = "$MUONTNP_PATH/Template/Input/addWeights.cxx"
 
 # -- path: must be absolute path
-generator.scriptName = "script_Reweight_106X.sh"
+# generator.scriptName = "script_Reweight_106X.sh"
+# generator.inputData = "/scratch/kplee/TagProbe/TnPTree/2016/MuonTriggerPaper/TnPTreeZ_07Aug17_SingleMuon_Run2016All_afterL1Fix.root"
+# generator.inputMC   = "/scratch/kplee/TagProbe/TnPTree/2020/TnPTreeZ_RelValZMM_13UP16_UL16_HighStat.root"
+# generator.outputMC  = "/scratch/kplee/TagProbe/TnPTree/2020/TnPTreeZ_RelValZMM_13UP16_UL16_HighStat_reweighted_afterL1Fix.root"
+# generator.Register()
+# generator.Generate()
+
+# generator.scriptName = "script_Reweight_106X_WrongL1.sh"
+# generator.inputData = "/scratch/kplee/TagProbe/TnPTree/2016/MuonTriggerPaper/TnPTreeZ_07Aug17_SingleMuon_Run2016All_afterL1Fix.root"
+# generator.inputMC   = "/scratch/kplee/TagProbe/TnPTree/2020/TnPTreeZ_RelValZMM_13UP16_UL16_HighStat_WrongL1.root"
+# generator.outputMC  = "/scratch/kplee/TagProbe/TnPTree/2020/TnPTreeZ_RelValZMM_13UP16_UL16_HighStat_WrongL1_reweighted_afterL1Fix.root"
+# generator.Register()
+# generator.Generate()
+
+# generator.scriptName = "script_Reweight_106X_postVFP.sh"
+# generator.inputData = "/scratch/kplee/TagProbe/TnPTree/2016/MuonTriggerPaper/TnPTreeZ_07Aug17_SingleMuon_Run2016All_afterL1Fix.root"
+# generator.inputMC   = "/scratch/kplee/TagProbe/TnPTree/2020/TnPTreeZ_RelValZMM_13UP16_UL16_HighStat_postVFP.root"
+# generator.outputMC  = "/scratch/kplee/TagProbe/TnPTree/2020/TnPTreeZ_RelValZMM_13UP16_UL16_HighStat_postVFP_reweighted_afterL1Fix.root"
+# generator.Register()
+# generator.Generate()
+
+generator.scriptName = "script_Reweight_106X_L1FixMarch_postVFP.sh"
 generator.inputData = "/scratch/kplee/TagProbe/TnPTree/2016/MuonTriggerPaper/TnPTreeZ_07Aug17_SingleMuon_Run2016All_afterL1Fix.root"
-generator.inputMC   = "/scratch/kplee/TagProbe/TnPTree/2020/TnPTreeZ_RelValZMM_13UP16_UL16_HighStat.root"
-generator.outputMC  = "/scratch/kplee/TagProbe/TnPTree/2020/TnPTreeZ_RelValZMM_13UP16_UL16_HighStat_reweighted_afterL1Fix.root"
+generator.inputMC   = "/scratch/kplee/TagProbe/TnPTree/2020/TnPTreeZ_RelValZMM_13UP16_UL16_HighStat_L1FixMarch_postVFP.root"
+generator.outputMC  = "/scratch/kplee/TagProbe/TnPTree/2020/TnPTreeZ_RelValZMM_13UP16_UL16_HighStat_L1FixMarch_postVFP_reweigthed.root"
 generator.Register()
 generator.Generate()
 
-generator.scriptName = "script_Reweight_106X_WrongL1.sh"
+generator.scriptName = "script_Reweight_106X_L1FixMarch_preVFP.sh"
 generator.inputData = "/scratch/kplee/TagProbe/TnPTree/2016/MuonTriggerPaper/TnPTreeZ_07Aug17_SingleMuon_Run2016All_afterL1Fix.root"
-generator.inputMC   = "/scratch/kplee/TagProbe/TnPTree/2020/TnPTreeZ_RelValZMM_13UP16_UL16_HighStat_WrongL1.root"
-generator.outputMC  = "/scratch/kplee/TagProbe/TnPTree/2020/TnPTreeZ_RelValZMM_13UP16_UL16_HighStat_WrongL1_reweighted_afterL1Fix.root"
-generator.Register()
-generator.Generate()
-
-generator.scriptName = "script_Reweight_106X_postFVP.sh"
-generator.inputData = "/scratch/kplee/TagProbe/TnPTree/2016/MuonTriggerPaper/TnPTreeZ_07Aug17_SingleMuon_Run2016All_afterL1Fix.root"
-generator.inputMC   = "/scratch/kplee/TagProbe/TnPTree/2020/TnPTreeZ_RelValZMM_13UP16_UL16_HighStat_postVFP.root"
-generator.outputMC  = "/scratch/kplee/TagProbe/TnPTree/2020/TnPTreeZ_RelValZMM_13UP16_UL16_HighStat_postVFP_reweighted_afterL1Fix.root"
+generator.inputMC   = "/scratch/kplee/TagProbe/TnPTree/2020/TnPTreeZ_RelValZMM_13UP16_UL16_HighStat_L1FixMarch_preVFP.root"
+generator.outputMC  = "/scratch/kplee/TagProbe/TnPTree/2020/TnPTreeZ_RelValZMM_13UP16_UL16_HighStat_L1FixMarch_preVFP_reweigthed.root"
 generator.Register()
 generator.Generate()
 
