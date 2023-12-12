a36020c323d119bb88457d9138e80f5729636c5d
이경필 <kplee@kplee-MacBookAir.local>
2023-12-12 16:44:30 +0100

    sync. with the backup codes

--- Include/SimplePlotTools.h
+++ Include/SimplePlotTools.h
@@ -619,7 +619,7 @@ public:
     graphInfos_.push_back( graphInfo );
   }
 
-  void Draw( TString drawOp = "EPSAME", TString saveOp = ".pdf" )
+  void Draw( TString drawOp = "EPSAME" )
   {
     if( !drawOp.Contains("SAME") ) drawOp = drawOp + "SAME";
 
@@ -663,7 +663,7 @@ public:
 
     DrawLatexAll();
 
-    c_->SaveAs(saveOp);
+    c_->SaveAs(".pdf");
   }
 };
 
@@ -671,9 +671,6 @@ class GraphCanvaswRatio: public GraphCanvas
 {
 public:
   vector<GraphInfo> graphInfoRatios_;
-
-  Bool_t removePointInRatio_;
-  Double_t ratio_threshold_;
 
   GraphCanvaswRatio()
   {
@@ -685,17 +682,9 @@ public:
     canvasName_ = canvasName;
     isLogX_ = isLogX;
     isLogY_ = isLogY;
-    removePointInRatio_ = kFALSE;
-    ratio_threshold_ = -999;
   }
 
-  void RemovePointInRatio_BelowThreshold(Double_t threshold)
-  {
-    removePointInRatio_ = kTRUE;
-    ratio_threshold_ = threshold;
-  }
-
-  void Draw( TString drawOp = "EPSAME", TString saveOp = ".pdf" )
+  void Draw( TString drawOp = "EPSAME" )
   {
     if( !drawOp.Contains("SAME") ) drawOp = drawOp + "SAME";
 
@@ -773,9 +762,8 @@ public:
     TF1 *f_line;
     PlotTool::DrawLine(f_line);
 
-    c_->SaveAs(saveOp);
+    c_->SaveAs(".pdf");
   }
-
 
   void CalcRatioGraph()
   {
@@ -790,25 +778,9 @@ public:
       Int_t color = graphInfos_[i].color;
 
       TGraphAsymmErrors *g_ratioTemp = MakeRatioGraph( g_target, g_ref );
-      if( removePointInRatio_ ) RemovePoint_BelowThreshold(g_ratioTemp, ratio_threshold_);
 
       GraphInfo graphInfoRatio{ g_ratioTemp, legend, color };
       graphInfoRatios_.push_back( graphInfoRatio );
-    }
-  }
-
-  void RemovePoint_BelowThreshold( TGraphAsymmErrors* g_ratio, Double_t threshold )
-  {
-    Int_t nPoint = g_ratio->GetN();
-    for(Int_t i_p=0; i_p<nPoint; i_p++)
-    {
-      Double_t x, y;
-      g_ratio->GetPoint( i_p, x, y );
-      if( x < threshold )
-      {
-        printf("[RemovePoint_BelowThreshold] (x, y) = (%lf, %lf) -> Removed\n", x, y);
-        g_ratio->SetPoint( i_p, x, 0 );
-      }
     }
   }
 
